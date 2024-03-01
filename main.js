9be1943a8e2805b1e04df65f3839b5fb1a1cdb10
nn-code80 <108633541+nn-code80@users.noreply.github.com>
2024-03-01 11:43:48 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const toggle = document.querySelector('.toggle-button')
 const navbar = document.querySelector('.nav-links2');
+const hamburger = document.querySelector('.hamburger');
+
 
 toggle.addEventListener('click', () => {
     navbar.classList.toggle('active');
+    hamburger.classList.toggle('open');
 })
 
 
